ed8a0fb6791ee97b7e6dd9860b87bc1e7ec81c7c
root <n.ashkenazi@f5.com>
2018-02-07 13:20:23 +0200

    intial script for importing asm policy
    Signed-off-by: root <n.ashkenazi@f5.com>

---
+++ Front-End-Back-End.js
@@ -0,0 +1,63 @@
+/**
+* A simple iControlLX extension that handles only HTTP GET
+*/
+function HelloWorld() {}
+
+HelloWorld.prototype.WORKER_URI_PATH = "ilxe_lab/hello_world";
+HelloWorld.prototype.isPublic = true;
+
+/**
+* Perform worker start functions
+*/
+
+HelloWorld.prototype.onStart = function(success, error) {
+
+  logger.info("HelloWorld onStart()");
+
+  var options = {
+    "method": "GET",
+    "hostname": "raw.githubusercontent.com",
+    "port": null,
+    "path": "/n8labs/super-netops/master/worker_state.json",
+    "headers": {
+      "cache-control": "no-cache"
+    }
+  };
+
+  var req = http.request(options, function (res) {
+
+    var chunks = [];
+
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    });
+
+    res.on("end", function () {
+      var body = Buffer.concat(chunks);
+      this.state = JSON.parse(body);
+    });
+  });
+
+  req.end();
+
+  success();
+};
+
+/**
+* handle onGet HTTP request
+*/
+HelloWorld.prototype.onGet = function(restOperation) {
+  restOperation.setBody(this.state);
+  this.completeRestOperation(restOperation);
+};
+
+/**
+* handle /example HTTP request
+*/
+HelloWorld.prototype.getExampleState = function () {
+  return {
+    "value": "your_string"
+  };
+};
+
+module.exports = HelloWorld;
