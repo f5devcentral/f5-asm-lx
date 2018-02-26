4035c5f165d9e36cf518188195b34a0b15ec892d
root <n.ashkenazi@f5.com>
2018-02-26 16:28:56 +0200

    Update

--- import_policy.js
+++ import_policy.js
@@ -1,55 +1,29 @@
-/**
- * A simple iControl LX extension that handles only HTTP GET
- */
-var logger = require('f5-logger').getInstance();
+logger = require('f5-logger').getInstance();
+
 function ImportPolicy() {}
 
 ImportPolicy.prototype.WORKER_URI_PATH = "asm/import_policy";
 ImportPolicy.prototype.isPublic = true;
 
-
-ImportPolicy.prototype.onStart = function(success, error) {
-
-    logger.info("ImportPolicy LX Started";
-
-    var options = {
-        "method": "GET",
-        "hostname": "raw.githubusercontent.com",
-        "port": null,
-        "path": "/nashkenazi/ASM-LX/master/Policy_Example.xml",
-        "headers": {
-            "cache-control": "no-cache"
-        }
-    };
-
-    var req = http.request(options, function (res) {
-
-        var chunks = [];
-
-        res.on("data", function (chunk) {
-            chunks.push(chunk);
-        });
-
-        res.on("end", function () {
-            var body = Buffer.concat(chunks);
-            this.state = JSON.parse(body);
-        });
-    });
-
-    req.end();
-    /*  logger.info("Recieve policy from SC: " + body);
-      sucess();
-    };
+ipam_extension.prototype.onStart = function (success) {
+    logger.info(WorkerName + " - onStart()");
+    success();
+};
 
 
-    /**
-    * handle onGet HTTP request
-    */
-
-    ImportPolicy.prototype.onGet = function(restOperation) {
-        restOperation.setBody(JSON.stringify( { value: "Hello World!" } ));
-        this.completeRestOperation(restOperation);
-    };
+ImportPolicy.prototype.onGet = function(restOperation) {
+    restOperation.setBody(JSON.stringify( { value: "Hello World!" } ));
+    this.completeRestOperation(restOperation);
+};
 
 /**
  * handle /example HTTP request
+ */
+ImportPolicy.prototype.getExampleState = function () {
+    return {
+        "value": "your_string"
+    };
+};
+
+module.exports = ImportPolicy;
+
