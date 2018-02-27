09e3257ff0af152857a8e2ac56466f7dc5467430
root <n.ashkenazi@f5.com>
2018-02-27 09:58:20 +0200

    Update

--- import_policy.js
+++ import_policy.js
@@ -21,6 +21,37 @@ ImportPolicy.prototype.onStart = function(success, error) {
 ImportPolicy.prototype.onPost = function(restOperation) {
     var newData = restOperation.getBody().SomeData;
     this.logger.info("SomeData is:"+newData);
+
+    var http = require("https");
+
+    var options = {
+        "method": "POST",
+        "hostname": "10.241.188.23",
+        "port": null,
+        "path": "/mgmt/tm/asm/policies",
+        "headers": {
+            "authorization": "Basic YWRtaW46YWRtaW4=",
+            "content-type": "application/json",
+            "cache-control": "no-cache",
+            "postman-token": "517ccc08-4f46-9c2f-86bb-cd84f96ae749"
+        }
+    };
+
+    var req = http.request(options, function (res) {
+        var chunks = [];
+
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+
+        res.on("end", function () {
+            var body = Buffer.concat(chunks);
+            console.log(body.toString());
+        });
+    });
+
+    req.write(JSON.stringify({ name: 'PolicyLXProfile' }));
+    req.end();
 };
 
 /**
